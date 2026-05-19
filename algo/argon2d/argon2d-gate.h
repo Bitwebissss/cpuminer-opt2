1024838e27b28bcd8ff96aa1e93805f6dbcd7c3b
Bitwebisss <196428942+Bitwebisss@users.noreply.github.com>
2026-05-19 15:12:13 +0300

    123

--- algo/argon2d/argon2d-gate.h
+++ algo/argon2d/argon2d-gate.h
@@ -39,4 +39,7 @@ bool register_argon2id1024_algo( algo_gate_t* gate );
 
 void argon2id1024_hash( void *state, const void *input );
 
+bool init_thread_argon2id1024(int thr_id);
+int  scanhash_argon2id1024_gpu(struct work*, uint32_t, uint64_t*, struct thr_info*);
+
 #endif
